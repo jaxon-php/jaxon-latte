563feb80832da94d6c748f757fe307d05d11b62a
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-08-18 06:38:04 +0200

    Fix.

--- src/View.php
+++ src/View.php
@@ -7,6 +7,10 @@ use Jaxon\App\View\ViewInterface;
 use Jaxon\App\View\ViewTrait;
 use Latte\Engine as TemplateEngine;
 
+use function substr;
+use function strlen;
+use function trim;
+
 class View implements ViewInterface
 {
     use ViewTrait;
@@ -18,13 +22,13 @@ class View implements ViewInterface
      *
      * @return string
      */
-    public function render(Store $store)
+    public function render(Store $store): string
     {
         $sViewName = $store->getViewName();
         $sNamespace = $store->getNamespace();
         // For this view renderer, the view name doesn't need to be prepended with the namespace.
         $nNsLen = strlen($sNamespace) + 2;
-        if(substr($sViewName, 0, $nNsLen) == $sNamespace . '::')
+        if(substr($sViewName, 0, $nNsLen) === $sNamespace . '::')
         {
             $sViewName = substr($sViewName, $nNsLen);
         }
